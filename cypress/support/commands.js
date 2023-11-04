fc394fb2b7990c0452be107df4bcc83313aa5409
donghun93 <alswn4516@gmail.com>
2023-11-04 22:15:01 +0900

    example

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,8 +16,8 @@ Cypress.Commands.add("checkPageLoadAndComponents", () => {
   // 컴포넌트 확인 (주요 버튼)
   cy.get('input[name="loginId"]').should("be.visible");
   cy.get('input[name="password"]').should("be.visible");
-//   cy.contains("비밀번호를 잊어버리셨나요?").click();
-  cy.get(".MuiStack-root > .MuiTypography-root").click();
+  cy.contains("비밀번호를 잊어버리셨나요?").click();
+  
 
 
   cy.url().should(
