21ff41b17149f33d89c35dc81c3b8e2bc4877db8
donghun93 <alswn4516@gmail.com>
2023-11-04 22:03:11 +0900

    'example'

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://accounts.elice.io/",
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 60000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
