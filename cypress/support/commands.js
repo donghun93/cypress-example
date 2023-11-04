79de80d09b4d5a5004f242f4a4181d6596c7e923
donghun93 <alswn4516@gmail.com>
2023-11-04 21:46:22 +0900

    'example'

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,43 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("checkPageLoadAndComponents", () => {
+  // 엘리스 로고 확인
+  cy.get(
+    'img[src="https://cdn-front-door.elice.io/accounts/static/media/elice_logo.3da193278ac05deb.png"]'
+  )
+    .should("have.attr", "alt", "header_logo")
+    .should("be.visible");
+
+  // 컴포넌트 확인 (주요 버튼)
+  cy.get('input[name="loginId"]').should("be.visible");
+  cy.get('input[name="password"]').should("be.visible");
+  cy.contains("비밀번호를 잊어버리셨나요?").click();
+
+  cy.url().should(
+    "include",
+    "/accounts/recover/password/find/email?continue_to=https%3A%2F%2Faccounts.elice.io%2F&lang=ko"
+  );
+
+  cy.visit("/");
+
+  cy.get('button[type="submit"]').should("be.visible");
+  cy.get('button[type="button"]')
+    .contains("카카오로 로그인")
+    .should("be.visible");
+  cy.get('button[type="button"]')
+    .contains("구글로 로그인")
+    .should("be.visible");
+
+  cy.contains("더보기").click();
+
+  // 추가 SNS 로그인 6종 [함수화]
+  cy.snsClickCheck([
+    "Microsoft",
+    "Facebook",
+    "Naver",
+    "Github",
+    "Apple",
+    "Whalespace",
+  ]);
+
+  // 회원가입 클릭
+  cy.contains("회원가입").click();
+});
