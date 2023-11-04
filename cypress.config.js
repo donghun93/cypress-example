3364e6efb97edff25ab922461c26881f3d0935aa
donghun93 <alswn4516@gmail.com>
2023-11-04 22:41:39 +0900

    'example'

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalStudio: true,
     baseUrl: "https://accounts.elice.io/",
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
--- cypress/e2e/1-getting-started/loginPage.cy.js
+++ cypress/e2e/1-getting-started/loginPage.cy.js
@@ -7,10 +7,10 @@ describe('앱 로드가 정상적으로 되어야 한다.', () => {
     cy.visit('/') //실행전 루트방문 init->commands
   })
 
-  it('비 로그인 시 로그인 페이지 이동 ', () => {
-    cy.clearCookie('eliceSessionKey')
-    cy.url().should('include', '/accounts/signin')
-  })
+  // it('비 로그인 시 로그인 페이지 이동 ', () => {
+  //   cy.clearCookie('eliceSessionKey')
+  //   cy.url().should('include', '/accounts/signin')
+  // })
 
   it("페이지 로드, 기본 요소 랜더딩 확인 ", () => {
     cy.checkPageLoadAndComponents(); // 페이지 로딩 및 컴포넌트 확인
